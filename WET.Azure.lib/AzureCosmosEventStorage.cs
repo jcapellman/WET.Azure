270b3b745dae6e211639c15904b8c3b9112a9c0f
Jarred Capellman <jcapellman@hotmail.com>
2021-02-25 19:29:50 -0600

    Added: Initial AzureCosmosEventStorage class

---
+++ WET.Azure.lib/AzureCosmosEventStorage.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Cosmos;
+
+using WET.lib.Containers;
+using WET.lib.Interfaces;
+
+namespace WET.Azure.lib
+{
+    public class AzureCosmosEventStorage : IEventStorage
+    {
+        private readonly CosmosClient _cosmosClient;
+        
+        private readonly Database _database;
+        
+        private readonly Container _container;
+
+        private readonly string _partitionKey;
+
+        public AzureCosmosEventStorage(string uri, string containerId, string databaseId, string primaryKey, string partitionKey)
+        {
+            _cosmosClient = new CosmosClient(uri, primaryKey);
+
+            _database = _cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId).Result;
+            _container = _database.CreateContainerIfNotExistsAsync(containerId, partitionKey).Result;
+
+            _partitionKey = partitionKey;
+        }
+
+        public async Task<bool> WriteEventAsync(ETWEventContainerItem item)
+        {
+            var result = await _container.CreateItemAsync(item, new PartitionKey(_partitionKey));
+
+            return result.StatusCode == HttpStatusCode.OK;
+        }
+
+        public void Shutdown()
+        {
+            _cosmosClient.Dispose();
+        }
+    }
+}
