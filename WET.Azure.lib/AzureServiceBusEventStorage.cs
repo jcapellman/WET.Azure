a0cab9c4466ceb698de0c97df2cb2ba2c0e5b6b3
Jarred Capellman <jcapellman@hotmail.com>
2021-02-25 20:33:23 -0600

    Added: AzureServiceBusEventStorage class

---
+++ WET.Azure.lib/AzureServiceBusEventStorage.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+
+using Azure.Messaging.ServiceBus;
+
+using WET.lib.Containers;
+using WET.lib.Interfaces;
+
+namespace WET.Azure.lib
+{
+    public class AzureServiceBusEventStorage : IEventStorage
+    {
+        private readonly string _connectionString;
+
+        private readonly string _queueName;
+
+        public AzureServiceBusEventStorage(string connectionString, string queueName)
+        {
+            _connectionString = connectionString;
+
+            _queueName = queueName;
+        }
+
+        public async Task<bool> WriteEventAsync(ETWEventContainerItem item)
+        {
+            await using var client = new ServiceBusClient(_connectionString);
+
+            var sender = client.CreateSender(_queueName);
+
+            var message = new ServiceBusMessage(System.Text.Json.JsonSerializer.Serialize(item));
+            
+            await sender.SendMessageAsync(message);
+
+            return true;
+        }
+
+        public void Shutdown()
+        {
+            // Nothing to do since tear down happens automatically
+        }
+    }
+}
