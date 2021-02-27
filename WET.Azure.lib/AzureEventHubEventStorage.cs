97fec9f25aca0df4ff7c5cc20b92fedb35f41896
Jarred Capellman <jcapellman@hotmail.com>
2021-02-27 16:56:14 -0600

    Added: Azure Event Hub Event Storage

---
+++ WET.Azure.lib/AzureEventHubEventStorage.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+using WET.lib.Containers;
+using WET.lib.Interfaces;
+
+namespace WET.Azure.lib
+{
+    public class AzureEventHubEventStorage : IEventStorage
+    {
+        private readonly string _connectionString;
+
+        private readonly string _eventHubName;
+
+        public AzureEventHubEventStorage(string connectionString, string eventHubName)
+        {
+            _connectionString = connectionString;
+
+            _eventHubName = eventHubName;
+        }
+        
+        public async Task<bool> WriteEventAsync(ETWEventContainerItem item)
+        {
+            await using var producerClient = new EventHubProducerClient(_connectionString, _eventHubName);
+
+            using var eventBatch = await producerClient.CreateBatchAsync();
+
+            eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(System.Text.Json.JsonSerializer.Serialize(item))));
+
+            await producerClient.SendAsync(eventBatch);
+
+            return true;
+        }
+
+        public void Shutdown()
+        {
+            // Not needed
+        }
+    }
+}
